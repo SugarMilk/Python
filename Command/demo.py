3924c8b0c8b6c017a9ea96c912f5cd711a9b45c3
SugarMilk <mr.huangjian@foxmail.com>
2017-07-05 00:23:59 +0800

    python执行shell

---
+++ Command/demo.py
@@ -0,0 +1,23 @@
+# coding: utf-8 python2.7
+# created by mr.huangjian@foxmail.com on 2017/7/4.
+
+import os
+
+"""
+/Users/huangjian/PycharmProjects/Python/Command
+statusCode: 0
+"""
+
+# No.1
+# statusCode = os.system("pwd")
+# print 'statusCode:', statusCode
+
+
+"""
+/Users/huangjian/PycharmProjects/Python/Command
+"""
+
+# No.2
+# f = os.popen("pwd")
+# print f.read()
+
